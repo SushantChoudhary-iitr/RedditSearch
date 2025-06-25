594594c4ffc606d90a230374df52d49bed4b8980
SushantChoudhary-iitr <sushant_c@ce.iitr.ac.in>
2025-06-25 18:40:47 +0530

    idk

--- server/routes/reddit.js
+++ server/routes/reddit.js
@@ -321,7 +321,7 @@ Stay natural. Avoid sounding like a pitch.
     
       const updatedUser = await User.findOneAndUpdate(
         { redditUsername }, // Find by redditUsername
-        { name, brandname, brandDescription, tone, industry, targetAudience, keySolution, notableResults, coreProblems }, // Fields to update
+        { name, brandname, brandDescription, industry, targetAudience, keySolution, notableResults, coreProblems }, // Fields to update
         { new: true, upsert: true } // Return updated doc, create if not exists
       );
     
